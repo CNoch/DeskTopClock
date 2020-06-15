72a3b2bea3d56cd98bcb3279e53baa7fbc1c61d5
CNoch <wr389110442@outlook.com>
2020-06-15 23:02:58 +0800

    添加时间

--- DeskTime/widget.cpp
+++ DeskTime/widget.cpp
@@ -1,11 +1,21 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDateTime>
+#include <QPainter>
+#include <windows.h>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this->setWindowFlag(Qt::FramelessWindowHint);
+    //this->setWindowFlag(Qt::WindowStaysOnTopHint);
+    this->setAttribute(Qt::WA_TranslucentBackground);
+    //m_TitleMoveWidget = new TitleMoveWidget();
+    m_Timer = new QTimer(this);
+    connect(m_Timer,&QTimer::timeout,this,&Widget::on_timeout);
+    m_Timer->start(1000);
 }
 
 Widget::~Widget()
@@ -13,3 +23,62 @@ Widget::~Widget()
     delete ui;
 }
 
+void Widget::paintEvent(QPaintEvent *event)
+{
+    QPainter p(this);
+    p.fillRect(this->rect(),QColor(0,0,0,50));
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton && m_InitialPoint_bool && m_Move_flag)
+    {
+        QPoint point = this->pos();
+        point.setX(point.x() + event->x() - m_InitialPoint.x());
+        point.setY(point.y() + event->y() - m_InitialPoint.y());
+        this->move(point);
+    }
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+    m_InitialPoint = event->pos();
+    m_InitialPoint_bool = true;
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *event)
+{
+    m_InitialPoint_bool = false;
+}
+
+void Widget::on_timeout()
+{
+    ui->label->setText(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
+}
+
+
+void Widget::on_pushButton_2_clicked()
+{
+    m_Move_flag = !m_Move_flag;
+    if (m_Move_flag)
+    {
+        ui->pushButton_2->setText(QStringLiteral("固定"));
+    }
+    else {
+        ui->pushButton_2->setText(QStringLiteral("取消固定"));
+    }
+}
+
+void Widget::on_pushButton_clicked()
+{
+    m_Top_flag = !m_Top_flag;
+    if (m_Top_flag)
+    {
+        ui->pushButton->setText(QStringLiteral("取消置顶"));
+        ::SetWindowPos(HWND(this->winId()),HWND_TOPMOST,0,0,0,0,SWP_NOMOVE| SWP_NOSIZE | SWP_SHOWWINDOW);
+    }
+    else {
+        ui->pushButton->setText(QStringLiteral("置顶"));
+        ::SetWindowPos(HWND(this->winId()), HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+    }
+}
--- DeskTime/widget.h
+++ DeskTime/widget.h
@@ -2,6 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTimer>
+#include <QPaintEvent>
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -15,7 +18,24 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+protected:
+    void paintEvent(QPaintEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+private slots:
+    void on_timeout();
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::Widget *ui;
+    QTimer *m_Timer = nullptr;
+    bool m_Move_flag = true;                //窗口移动
+    bool m_InitialPoint_bool = false;       //鼠标点击标志位（用于进行窗口移动）
+    QPoint  m_InitialPoint;                 //鼠标点击初始点（用于进行窗口移动）
+
+    bool m_Top_flag = false;                 //窗口置顶
 };
 #endif // WIDGET_H
